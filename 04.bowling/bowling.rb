110ce69263cf4035562b89aadc78822da4842210
takuya3769 <dasgquhf11@gmail.com>
2022-04-12 21:33:03 +0900

    プログラム再修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-LASTFRAME = 18
+LAST_FRAME = 18
 score = ARGV[0]
 scores = score.split(',')
 shots = []
 scores.each do |s|
-  if shots.length >= LASTFRAME && s == 'X'
+  if shots.length >= LAST_FRAME && s == 'X'
     shots << 10
   elsif s == 'X'
     shots << 10
@@ -25,26 +25,22 @@ shots.each_slice(2) do |shot|
 end
 
 point = frames.each_with_index.sum do |frame, i|
-    next_frame = frames[i + 1]
-    after_next_frame = frames[i + 2]
-    strike = frame[0] == 10
-    spare = !strike && frame.sum == 10
+  next_frame = frames[i + 1]
+  after_next_frame = frames[i + 2]
+  strike = frame[0] == 10
+  spare = !strike && frame.sum == 10
 
-    if i == 9
-      frame.sum
-    elsif strike
-      double = next_frame[0] == 10
-      if double
-        second_point = i == 8 ? next_frame[1] : after_next_frame[0]
-        next_frame[0] + second_point + 10
-      else
-        next_frame[0] + next_frame[1] + 10
-      end
-    elsif spare
-      next_frame[0] + 10
-    else
-      frame.sum
-    end
+  if i == 9
+    frame.sum
+  elsif strike
+    double = next_frame[0] == 10
+    second_point = double && i < 8 ? after_next_frame[0] : next_frame[1]
+    next_frame[0] + second_point + 10
+  elsif spare
+    next_frame[0] + 10
+  else
+    frame.sum
+  end
 end
 
 puts point
