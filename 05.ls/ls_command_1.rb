0afa98d46186237ac0a89981d6003f55e148cf50
takuya3769 <dasgquhf11@gmail.com>
2022-05-02 16:42:30 +0900

    指摘箇所修正

--- 05.ls/ls_command_1.rb
+++ 05.ls/ls_command_1.rb
@@ -7,19 +7,20 @@ directory = Dir.glob('*')
 MAX_CLUMN = 3
 MAXIMUM_FILE = directory.length.to_f
 row = (MAXIMUM_FILE / MAX_CLUMN).ceil
-if (MAX_CLUMN * row - MAXIMUM_FILE) == 1
-  directory.push(nil)
-elsif (MAX_CLUMN * row - MAXIMUM_FILE) == 2
-  directory.push(nil)
-  directory.push(nil)
+if (MAX_CLUMN % MAXIMUM_FILE) != 0
+  (MAX_CLUMN * row - MAXIMUM_FILE).to_i.times do
+    directory.push(nil)
+  end
 end
 
 def show_directories(directory, row)
   file_list = directory.each_slice(row).to_a
+  longest_name = file_list.max_by(&:size)
+  padding = 15
   files = file_list.transpose
   files.each do |list|
     list.each do |file|
-      print file.to_s.ljust(24)
+      print file.to_s.ljust(longest_name.size + padding)
     end
     print "\n"
   end
