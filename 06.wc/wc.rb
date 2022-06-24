614f5b124a95e924198c5a89b02730f1088bd952
takuya3769 <dasgquhf11@gmail.com>
2022-06-24 23:15:14 +0900

    指摘箇所修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -4,59 +4,58 @@ require 'optparse'
 
 def main
   params = ARGV.getopts('l')
-  files_data = take_files
-  total_files = sum_files(files_data)
-  files_data.empty? ? basic_input(params) : type_l_command(params, files_data, total_files)
+  collect_files = take_file
+  total_files = sum_files(collect_files)
+  collect_files.empty? ? enter_standard_input(params) : enter_l_option(params, collect_files, total_files)
 end
 
-def basic_input(params)
+def enter_standard_input(params)
   standard_input = $stdin.read
   print standard_input.count("\n").to_s.rjust(8).to_s
   puts "#{standard_input.split(/\s+/).size.to_s.rjust(8)}#{standard_input.bytesize.to_s.rjust(7)}" unless params['l']
-  print "\n"
 end
 
-def take_files
-  files_data = []
-  ARGV.each do |information|
-    read_file = File.read(information)
+def take_file
+  collect_files = []
+  ARGV.each do |name|
+    read_file = File.read(name)
     file_data = {
       line: read_file.count("\n"),
       word: read_file.split(/\s+/).size,
       byte: read_file.bytesize,
-      filename: File.basename(information)
+      filename: File.basename(name)
     }
-    files_data << file_data
+    collect_files << file_data
   end
-  files_data
+  collect_files
 end
 
-def sum_files(files_data)
+def sum_files(collect_files)
   {
-    total_line: files_data.sum { |lines| lines[:line] },
-    total_word: files_data.sum { |words| words[:word] },
-    total_byte: files_data.sum { |bytes| bytes[:byte] }
+    total_line: collect_files.sum { |lines| lines[:line] },
+    total_word: collect_files.sum { |words| words[:word] },
+    total_byte: collect_files.sum { |bytes| bytes[:byte] }
   }
 end
 
-def type_l_command(params, files_data, total_files)
-  params['l'] ? show_lines(files_data, total_files) : show_files(files_data, total_files)
+def enter_l_option(params, collect_files, total_files)
+  params['l'] ? show_lines(collect_files, total_files) : show_files(collect_files, total_files)
 end
 
-def show_files(files_data, total_files)
-  files_data.each do |file|
+def show_files(collect_files, total_files)
+  collect_files.each do |file|
     puts "#{file[:line].to_s.rjust(8)} #{file[:word].to_s.rjust(7)} #{file[:byte].to_s.rjust(7)} #{file[:filename]}"
   end
-  return unless files_data.size != 1
+  return unless collect_files.size != 1
 
   puts "#{total_files[:total_line].to_s.rjust(8)} #{total_files[:total_word].to_s.rjust(7)} #{total_files[:total_byte].to_s.rjust(7)} total"
 end
 
-def show_lines(files_data, total_files)
-  files_data.each do |file|
+def show_lines(collect_files, total_files)
+  collect_files.each do |file|
     puts "#{file[:line].to_s.rjust(8)} #{file[:filename]}"
   end
-  puts "#{total_files[:total_line].to_s.rjust(8)} total" unless files_data.size == 1
+  puts "#{total_files[:total_line].to_s.rjust(8)} total" unless collect_files.size == 1
 end
 
 main
